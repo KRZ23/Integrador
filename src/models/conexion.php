84f3354feb73e32324607d24afb3b230e6addd61
KRZ23 <cristopherspier23@gmail.com>
2024-10-26 17:14:59 -0500

    Scrip que da Mysql para la conexion a la base de datos
    Lo comente por si el primero no funcione y usar el segundo

---
+++ src/models/conexion.php
@@ -0,0 +1,28 @@
+<?php
+
+class conexion{
+    private $user;
+    private $password;
+    private $server;
+    private $database;
+    private $con;
+
+    public function __construct(){
+        $user ='root';
+        $password=''; //por definir
+        $server='localhost';
+        $database=''; //por definir 
+        $this->con = new mysqli($server,$user,$password,$database);
+    }
+}
+
+//$host="localhost";
+//$port=3306;
+//$socket="";
+//$user="root";
+//$password="";
+//$dbname="bd_piedradeagua";
+
+//$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	//or die ('Could not connect to the database server' . mysqli_connect_error());
+//$con->close();
