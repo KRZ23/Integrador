26dc053d793218c5195a143b9b826ddf635cdc96
KRZ23 <cristopherspier23@gmail.com>
2024-10-29 23:29:01 -0500

    Tratamiendo de roles
    Aun se tienen crear las vistas

--- src/Controller/AccsesUsers.php
+++ src/Controller/AccsesUsers.php
@@ -16,8 +16,38 @@ $buscarUsuario = $con->getUser($usuario,$password);
 
 foreach($buscarUsuario as $user){
     $id_cliente = $user['nombre_cliente'];
+    $dni=$user['dni'];
     $correo = $user['correo_cliente'];
+    $telef =$user['tel_cliente'];
     $rol = $user['id_rol'];
     $password = $user['contrasena'];
     $edad =$user['edad'];
+}
+
+if (empty($searchUser)) {
+    echo '
+        <script language="javascript">
+            alert("Usuario o Contraseña incorrectos, por favor intenta de nuevo");
+            self.location = "/index.php";
+        </script>
+    ';
+} else {
+    switch ($rol) {
+        case 'Cliente':
+            require(''); // Aún no se crea la vista del cliente
+            break;
+        case 'Jefe':
+            require(''); // Aún no se crea la vista del jefe
+            break;
+        case 'Carguero':
+            require(''); // Aún no se crea la vista del carguero
+            break;
+        case 'Seguridad':
+            require(''); // Aún no se crea la vista del seguridad
+            break;
+        default:
+            // Manejo de rol no reconocido
+            echo '<script>alert("Rol no reconocido"); self.location = "/index.php";</script>';
+            break;
+    }
 }
